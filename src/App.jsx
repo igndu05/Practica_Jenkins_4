ed6488c89bdcd48933b26d6893523f2e658c7f34
Ignacio Durán <idurmes337@g.educaand.es>
2025-12-17 12:15:08 +0100

    Add message indicating change made

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>Cambio hecho</p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
